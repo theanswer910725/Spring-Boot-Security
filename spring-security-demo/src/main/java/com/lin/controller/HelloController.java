954ca64aa3b0df601a02a8b7e7ca9688102f6dcb
Lin. <work_lin@foxmail.com>
2019-07-29 22:34:21 +0800

    pom父依赖改为spring boot，增加测试controller

---
+++ spring-security-demo/src/main/java/com/lin/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.lin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello spring security";
+	}
+}
